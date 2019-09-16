5962abd9f4c3665bf8c6245650557d3cfe4ad8a1
globefire <sahun5774@gmail.com>
2019-09-17 01:42:26 +0530

    scrapping successful

--- mediumscrapper/home/views.py
+++ mediumscrapper/home/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render, redirect
 
 from .forms import NameForm
 
+import requests as req
+import bs4
+import warnings
+warnings.simplefilter('ignore') 
+
 def get_link(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -13,15 +18,22 @@ def get_link(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            text = form.cleaned_data['urlink']
-            print(str(text))
+            val = form.cleaned_data['urlink']
+            print(str(val))
+            res = req.get(val)
+            soup = bs4.BeautifulSoup(res.text, 'lxml')
+            content = ''
+            for i in soup.select('article'):
+                content += i.getText()
+
+            val = content
             form = NameForm()
             # return redirect('home:home')
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = NameForm()
-        text = 'first time'
-        print(str(text))
+        val = 'first time'
+        print(str(val))
 
-    return render(request, 'home/index.html', {'form': form, 'text':text})
+    return render(request, 'home/index.html', {'form': form, 'text':val})
